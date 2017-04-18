87e240df3934c9ccab1d4fb02e7625489c33b762
Justin Howard <jmhoward0@gmail.com>
2017-04-17 21:35:05 -0700

    Require gem in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,8 @@ if ENV['COVERAGE']
   end
 end
 
+require 'sqs_worker'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
